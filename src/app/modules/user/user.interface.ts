09bb9b1a8d7a2cd827f629072bbf73f9ec671f63
Arifur Rahman <arifur3429@gmail.com>
2026-01-14 22:35:55 +0600

    user model done

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,41 +1,34 @@
-import { Types } from "mongoose";
-
 export enum Role {
-    SUPER_ADMIN = "SUPER_ADMIN",
-    ADMIN = "ADMIN",
-    USER = "USER",
-    GUIDE = "GUIDE",
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  EMPLOYEE = "EMPLOYEE",
 }
 
 //auth providers
-/**
- * email, password 
- * google authentication
- */
-
 export interface IAuthProvider {
-    provider: string;  // "Google", "Credential"
-    providerId: string;
+  provider: string; // "Google", "Credential"
+  providerId: string;
 }
 
 export enum IsActive {
-    ACTIVE = "ACTIVE",
-    INACTIVE = "INACTIVE",
-    BLOCKED = "BLOCKED"
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
 }
 
 export interface IUser {
-    name: string;
-    email: string;
-    password?: string;
-    phone?: string;
-    picture?: string;
-    address?: string;
-    isDeleted?: string;
-    isActive?: IsActive;
-    isVerified?: string;
-    role: Role;
-    auths: IAuthProvider[]
-    bookings?: Types.ObjectId[]
-    guides?: Types.ObjectId[]
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: Role;
+  auths: IAuthProvider[];
+  //   bookings?: Types.ObjectId[];
+  //   guides?: Types.ObjectId[];
 }
